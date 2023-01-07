5026a0775e98418d01860bfac77fe2d9def54ef4
Alex <Alexander.S_97@gmx.de>
2023-01-07 13:45:16 +0700

    added ladder and climbing ladder up

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,9 +8,12 @@ public class PlayerMovement : MonoBehaviour
 {
     public float movementSpeed = 0.5f;
     public Tilemap map;
+    public Tilemap background;
+    public TileBase ladder;
     private PlayerInput playerInput;
     private Vector2 moveDirection = Vector2.zero;
     private Vector3 velocity = Vector3.zero;
+    private bool isMoving;
     private Rigidbody2D rb2D;
 
     private void Awake() => playerInput = new PlayerInput();
@@ -21,14 +24,21 @@ public class PlayerMovement : MonoBehaviour
     {
         rb2D = GetComponent<Rigidbody2D>();
         playerInput.Player.Move.performed += ctx => Move(ctx);
+        playerInput.Player.Move.started += ctx => SetIsMoving(ctx);
+        playerInput.Player.Move.canceled += ctx => SetIsNotMoving(ctx);
         playerInput.Player.Fire.performed += ctx => Dig(ctx);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (isMoving) {
+            Move(new CallbackContext());
+        }
     }
 
+    private void SetIsMoving(CallbackContext context) => isMoving = true;
+    private void SetIsNotMoving(CallbackContext context) => isMoving = false;
 
     void Move(CallbackContext context) {
         moveDirection = playerInput.Player.Move.ReadValue<Vector2>();
@@ -43,23 +53,33 @@ public class PlayerMovement : MonoBehaviour
         }
         if (moveDirection.y < 0) {
             //Look down
+            //if Ladder climb down else dig
         }
         if (moveDirection.y > 0) {
             //Look up;
+            //if Ladder climb up
+            Vector3Int gridPosition = map.WorldToCell(transform.position);
+            if (background.HasTile(gridPosition)) {
+                Debug.Log("Move up!");
+                Vector3 targetVelocity = new Vector2(rb2D.velocity.x, moveDirection.y * movementSpeed);
+                rb2D.velocity = targetVelocity;
+            }
         }
     }
 
     void Dig(CallbackContext context) {
         // Find the tile we are looking at
         Vector2 tileUnderneath = new Vector2(transform.position.x, transform.position.y) + moveDirection;
-        Debug.DrawLine(transform.position, tileUnderneath,Color.red,4);
         // Translate worldPosition to gridPosition
         Vector3Int gridPosition = map.WorldToCell(tileUnderneath);
         // Remove looked at tile
         // Check if position is on a tile
         if (map.HasTile(gridPosition)) {
-            Debug.Log("Remove Tile!");
             map.SetTile(gridPosition,null);
+        }
+        // If direction was down place a ladder
+        if (moveDirection.y < 0) {
+            background.SetTile(gridPosition,ladder);
         }
     }
 }
